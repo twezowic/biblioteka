d01d584559cc11e5eb8b85bf7778c0dd38467351
twezowic <01169258@pw.edu.pl>
2023-01-17 16:50:34 +0100

    Move dropping tables.

---
+++ drop.sql
@@ -0,0 +1,14 @@
+DROP table penalties_history;
+DROP table penalties;
+DROP table orders_history;
+DROP table orders;
+DROP table USERS;
+DROP table users_data;
+DROP table copies;
+DROP table books;
+DROP table authors;
+DROP table work_times;
+DROP table libraries;
+DROP table addresses;
+
+Commit;
